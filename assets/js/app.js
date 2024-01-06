aec2c0a4802be01f69b756c461db77b7b969d8e9
nathius262 <egbodonathaniel@yahoo.com>
2024-01-06 16:15:40 +0100

    url description test

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,18 +16,7 @@ postEl.insertAdjacentHTML('afterbegin', post())
 
 
 head.insertAdjacentHTML('afterbegin', `
-    <meta charset="utf-8">
-    <meta name="description" content="Transforming Ideas into Immersive Experiences through Innovative Programming">
-    <meta name="keywords" content="pixtinifinty, pixet, pixt, infinity">
-    <meta name="author" content="pixtinifinty">
-    <meta property="og:title" content="Pixtinfinity- Programing Ideas Into Experience">
-    <meta property="og:description" content="Transforming Ideas into Immersive Experiences through Innovative Programming">
-    <meta property="og:image" content="assets/images/logo-footer.png">
-    <meta property="og:url" content="https://pixtinfinity.com/">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="assets/images/favicon.ico">
-    <title>PIXTINFINITY</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet"> 
+     
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/fontawesome.min.css" integrity="sha512-siarrzI1u3pCqFG2LEzi87McrBmq6Tp7juVsdmGY1Dr8Saw+ZBAzDzrGwX3vgxX1NkioYNCFOVC0GpDPss10zQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
